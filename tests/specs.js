5a68e4b6f4e76a3c517c704ad02f4fef24fc4586
igor <deadbyelpy@gmail.com>
2016-08-01 11:35:00 +0300

    add tests for multiple watchers

--- tests/specs.js
+++ tests/specs.js
@@ -24,11 +24,70 @@ describe('main', function () {
             name.should.equal('fld');
             oldValue.should.equal(128);
             newValue.should.equal(256);
-            console.log('done');
             done();
         });
 
         obj.fld = 256;
+    });
+
+    it('should pass: modify fld value, 2 watchers', function ( done ) {
+        var obj = {
+            fld: 222
+        };
+
+        watch(obj, 'fld', function ( name, oldValue, newValue ) {
+            oldValue.should.equal(222);
+            newValue.should.equal(333);
+        });
+
+        watch(obj, 'fld', function ( name, oldValue, newValue ) {
+            oldValue.should.equal(222);
+            newValue.should.equal(333);
+            done();
+        });
+
+        obj.fld = 333;
+    });
+
+    it('should pass: modify fld value, 2 watchers', function ( done ) {
+        var obj = {
+            fld: 222
+        };
+
+        watch(obj, 'fld', function ( name, oldValue, newValue ) {
+            oldValue.should.equal(222);
+            newValue.should.equal(333);
+        });
+
+        watch(obj, 'fld', function ( name, oldValue, newValue ) {
+            oldValue.should.equal(222);
+            newValue.should.equal(333);
+            done();
+        });
+
+        obj.fld = 333;
+    });
+
+    it('should pass: modify fld value, 2 watchers, 1 removed in runtime', function ( done ) {
+        var obj = {
+                fld: 111
+            },
+            removedWatcher;
+
+        watch(obj, 'fld', function ( name, oldValue, newValue ) {
+            oldValue.should.equal(111);
+            newValue.should.equal(222);
+            done();
+        });
+
+        removedWatcher = watch(obj, 'fld', function ( name, oldValue, newValue ) {
+            oldValue.should.equal(111);
+            newValue.should.equal(222);
+            done();
+        });
+        removedWatcher.remove();
+
+        obj.fld = 222;
     });
 
 });
